f4a13600b0b4926c872268c7c3dd6ae069bf2f24
Wayne Tong <dkotrzarashi@gmail.com>
2023-03-29 21:52:04 -0400

    Add 49. Group Anagrams solution

---
+++ 49. Group Anagrams/group-anagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    let map = new Map();
+    for (let str of strs) {
+        let sorted = str.split('').sort().join('');
+        if (!map.has(sorted)) {
+            map.set(sorted, [str]);
+        } else {
+            map.get(sorted).push(str);
+        }
+    }
+    return Array.from(map.values());
+};

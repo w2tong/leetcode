25f913727b1f9821b6d4c8ca8905739e1543d278
Wayne Tong <dkotrzarashi@gmail.com>
2023-03-30 21:30:41 -0400

    Add 1. Two Sum solution

---
+++ 1. Two Sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            return [map.get(nums[i]), i];
+        }
+        map.set(target - nums[i], i);
+    }
+    return [-1, -1];
+};

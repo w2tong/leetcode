209354def6a3dc15f0dcdd5ff37ff75e0c62ad88
Wayne Tong <dkotrzarashi@gmail.com>
2023-04-17 19:35:46 -0400

    Add 14. Longest Common Prefix solution

---
+++ 14. Longest Common Prefix/longest-common-prefix.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    strs.sort();
+    for (let i = 0; i < strs[0].length; i++) {
+        if (strs[0][i] !== strs[strs.length - 1][i]) return strs[0].slice(0, i);
+    }
+    return strs[0];
+};

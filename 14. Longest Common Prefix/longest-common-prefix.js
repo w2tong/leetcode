5dc4bed381d4645347e57a58178fc16b0ed2d51c
Wayne Tong <dkotrzarashi@gmail.com>
2023-04-17 19:41:20 -0400

    Add 2nd 14. Longest Common Prefix solution

--- 14. Longest Common Prefix/longest-common-prefix.js
+++ 14. Longest Common Prefix/longest-common-prefix.js
@@ -9,3 +9,14 @@ var longestCommonPrefix = function (strs) {
     }
     return strs[0];
 };
+
+var longestCommonPrefix2 = function (strs) {
+    for (let c = 0; c < strs[0].length; c++) {
+        for (let i = 1; i < strs.length; i++) {
+            if (c >= strs[i].length || strs[0][c] !== strs[i][c]) {
+                return strs[0].slice(0, c);
+            }
+        }
+    }
+    return strs[0];
+};

3b221ff6c90465081f08d3cdde6eab92a5600451
Wayne Tong <dkotrzarashi@gmail.com>
2023-05-11 18:51:06 -0400

    Add 110. Balanced Binary Tree solution

---
+++ 110. Balanced Binary Tree/balanced-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+    return helper(root) !== -1;
+};
+
+var helper = function (node) {
+    if (!node) return 0;
+    const left = helper(node.left);
+    const right = helper(node.right);
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+        return -1;
+    }
+    return Math.max(left, right) + 1;
+}

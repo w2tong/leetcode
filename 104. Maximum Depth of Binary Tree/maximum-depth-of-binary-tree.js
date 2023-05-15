98d0d3350ebe9beae4e49cc7c1efbb8f73422d84
Wayne Tong <dkotrzarashi@gmail.com>
2023-05-15 16:27:41 -0400

    Add 104. Maximum Depth of Binary Tree BFS solution

--- 104. Maximum Depth of Binary Tree/maximum-depth-of-binary-tree.js
+++ 104. Maximum Depth of Binary Tree/maximum-depth-of-binary-tree.js
@@ -10,9 +10,25 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+// DFS
 var maxDepth = function (root) {
-    if (!root) {
-        return 0;
-    }
+    if (!root) return 0;
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+// BFS
+var maxDepth2 = function (root) {
+    let queue = [root];
+    let height = 0;
+    while (queue[0]) {
+        const len = queue.length;
+        for (let i = 0; i < len; i++) {
+            const node = queue.shift();
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        height++;
+    }
+    return height;
 };

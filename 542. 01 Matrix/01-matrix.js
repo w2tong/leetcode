97f108ffe63b6fcf0b4d6b6ff1f0cf9b888c7fce
Wayne Tong <dkotrzarashi@gmail.com>
2023-05-16 21:30:18 -0400

    Add 542. 01 Matrix DP solution

--- 542. 01 Matrix/01-matrix.js
+++ 542. 01 Matrix/01-matrix.js
@@ -2,6 +2,37 @@
  * @param {number[][]} mat
  * @return {number[][]}
  */
+
+// Dynamic Programming
+var updateMatrix = function (mat) {
+    const m = mat.length;
+    const n = mat[0].length;
+    const INF = m + n;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let top = INF, left = INF;
+            if (mat[i][j] === 0) continue;
+            if (i - 1 >= 0) top = mat[i - 1][j]
+            if (j - 1 >= 0) left = mat[i][j - 1]
+            mat[i][j] = Math.min(top, left) + 1;
+        }
+    }
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            if (mat[i][j] === 0) continue;
+            let bot = INF, right = INF;
+            if (i + 1 < m) bot = mat[i + 1][j]
+            if (j + 1 < n) right = mat[i][j + 1]
+            mat[i][j] = Math.min(mat[i][j], Math.min(bot, right) + 1);
+        }
+    }
+
+    return mat;
+};
+
+// BFS
 var updateMatrix = function (mat) {
     const m = mat.length;
     const n = mat[0].length;
